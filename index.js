70ea5060933cd82829c40bcdf783966a99b7260f
Nobert-Muma <mumanobert17@gmail.com>
2024-07-08 19:31:16 +0300

    Js Event Listeners

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ function addingEventListener() {
     let input;
     input =document.getElementById("button");
     input.addEventListener('click', function(){
-
+        alert('I was clicked!');
     })
 }
+addingEventListener()
